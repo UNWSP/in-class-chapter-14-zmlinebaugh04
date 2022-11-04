27e574be60cdfd75847915d9fb1ebe5cfca7766c
zmlinebaugh04 <zmlinebaugh@students.unwsp.edu>
2022-11-03 19:57:09 -0500

    Completed

---
+++ src/JHello.java
@@ -0,0 +1,9 @@
+
+public class JHello {
+
+	public static void main(String[] args) {
+		JHelloFrame frame = new JHelloFrame();
+		frame.setVisible(true);
+	}
+
+}
---
+++ src/Jframe1.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+public class Jframe1 {
+
+	public static void main(String args[]) {
+		JFrame aFrame = new JFrame("First Frame");
+		aFrame.setSize(250, 100);
+		aFrame.setVisible(true);
+		
+	}
+
+}
